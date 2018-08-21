598a16ea101c2bd6e4bab8e79dd05acf497f5bf4
1733856225@qq.com <1733856225@qq.com>
2018-08-21 21:17:32 +0800

    实体类提交

--- src/main/java/com/picc/riskcontrol/riskinfo/vo/response/RiskInfoResponseVo.java
+++ src/main/java/com/picc/riskcontrol/riskinfo/vo/response/RiskInfoResponseVo.java
@@ -2,4 +2,13 @@ package com.picc.riskcontrol.riskinfo.vo.response;
 
 public class RiskInfoResponseVo {
 
+	private  String errorMessage;
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
 }
---
+++ src/main/java/com/picc/riskcontrol/riskinfo/web/RiskInfoAction.java
@@ -0,0 +1,30 @@
+package com.picc.riskcontrol.riskinfo.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.picc.riskcontrol.riskinfo.service.facade.RiskInfoService;
+import com.picc.riskcontrol.riskinfo.vo.response.RiskInfoResponseVo;
+
+@Controller
+@RequestMapping("/riskinfo")
+public class RiskInfoAction {
+	@Autowired
+	RiskInfoService riskInfoService;
+	
+	// 必须使用spring-web4.2以上的版本，否则不能导入
+	@CrossOrigin(origins = "*", maxAge = 3600)
+	@RequestMapping(value="/queryClaimInfo",method={RequestMethod.GET,RequestMethod.POST})
+	@ResponseBody
+	public RiskInfoResponseVo queryRiskInfoClaim(){
+		RiskInfoResponseVo riskInfoResponseVo=new RiskInfoResponseVo();
+		System.out.println("小子，成功了没22");
+		String claim="success222";
+		riskInfoResponseVo.setErrorMessage(claim);
+		return riskInfoResponseVo;
+	}
+}
